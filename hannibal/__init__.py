5225f461f624e2b928ffbd64bad87bfdaadd18f4
月珑沙 <avalon852456@gmail.com>
2018-11-27 16:32:00 +0800

    packaging bug fix

--- hannibal/__init__.py
+++ hannibal/__init__.py
@@ -1,3 +1,9 @@
-from hannibal.parser import IncreasingParser, BaseParser
-from hannibal.spider import DistributeCollector, LocalCollector
-from hannibal.util import RedisQueue, RedisHrefPool, MemPool, MemQueue, TrickHelper, extract_json, extract_html, Mission
+__version__ = '0.0.8'
+
+from hannibal.parser.parser import IncreasingParser, BaseParser
+from hannibal.spider.distribute_collector import DistributeCollector
+from hannibal.spider.local_collector import LocalCollector
+from hannibal.util.queues import RedisQueue, MemQueue
+from hannibal.util.tricks import TrickHelper
+from hannibal.util.href_pool import RedisHrefPool, MemPool
+from hannibal.util.common_utils import Mission, extract_html, extract_json
